1cec61e074ec90d64911b0783486a6bc23303806
Fabian <fkaiser@student.ethz.ch>
2015-03-27 09:24:10 +0100

    Second commit

---
+++ lepton_camera/src/video/lepton.cpp
@@ -0,0 +1,187 @@
+/*
+ Copyright (c) 2014, Pure Engineering LLC
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+#include <limits.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/Int32MultiArray.h"
+#include "sensor_msgs/Image.h"
+#include "SPI.h"
+
+#include <sstream>
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+static void pabort(const char *s) {
+	perror(s);
+	abort();
+}
+
+#define VOSPI_FRAME_SIZE (164)
+uint8_t lepton_frame_packet[VOSPI_FRAME_SIZE];
+static unsigned int lepton_image[80][80] = { 0 };
+
+int transfer(int fd, int current_frame) {
+	int ret;
+	int i;
+	int frame_number;
+	uint8_t tx[VOSPI_FRAME_SIZE] = { 0, };
+
+	struct spi_ioc_transfer tr;
+
+	tr.tx_buf = (unsigned long) tx;
+	tr.rx_buf = (unsigned long) lepton_frame_packet;
+	tr.len = VOSPI_FRAME_SIZE;
+	tr.delay_usecs = spi_delay;
+	tr.speed_hz = spi_speed;
+	tr.bits_per_word = spi_bitsPerWord;
+
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+
+	if (ret < 1)
+		pabort("can't send spi message");
+
+	if (((lepton_frame_packet[0] & 0xf) != 0x0f)) {
+		frame_number = lepton_frame_packet[1];
+
+		//printf("%d", frame_number);
+
+		if (frame_number == current_frame) {
+			for (i = 0; i < 80; i++) {
+				lepton_image[frame_number][i] = (lepton_frame_packet[2 * i + 4]
+						<< 8 | lepton_frame_packet[2 * i + 5]);
+			}
+		} else {
+			frame_number = -1;
+		}
+	}
+	return frame_number;
+}
+
+sensor_msgs::Image normalize(sensor_msgs::Image I,
+		unsigned int Data_raw[80][80]) {
+
+	int i;
+	int j;
+	unsigned int maxval = 0;
+	unsigned int minval = UINT_MAX;
+
+	// Calculating extrema
+	for (i = 0; i < 60; i++) {
+		for (j = 0; j < 80; j++) {
+			if (Data_raw[i][j] > maxval) {
+				maxval = Data_raw[i][j];
+			}
+			if (Data_raw[i][j] < minval) {
+				minval = Data_raw[i][j];
+			}
+		}
+	}
+	float diff = maxval - minval;
+	if (diff == 0.0) {
+		diff = 1.0;
+	}
+	float scale = 255 / diff;
+
+	// File array with normalized pixel points
+	for (i = 0; i < 80; i++) {
+		for (j = 0; j < 60; j++) {
+			I.data[j * 80 + i] = (Data_raw[j][i] - minval) * scale;
+		}
+	}
+
+	return I;
+}
+
+int main(int argc, char *argv[]) {
+
+	int i, j;
+	ros::init(argc, argv, "arrayPublisher");
+	ros::NodeHandle n;
+	ros::Publisher pub = n.advertise<std_msgs::Int32MultiArray>("array", 100);
+	ros::Publisher data = n.advertise<sensor_msgs::Image>("IR_data", 100);
+
+	sensor_msgs::Image IR_data;
+	IR_data.height = 60;
+	IR_data.width = 80;
+	IR_data.step = 80;
+	IR_data.encoding = "mono8";
+
+	int ret = 0;
+	int reset = 0;
+	while (ros::ok()) {
+
+		IR_data.data.clear();
+		for (j = 0; j < (int) IR_data.height; j++) {
+			for (i = 0; i < (int) IR_data.width; i++) {
+				IR_data.data.push_back(0);
+			}
+		}
+		//open spi port
+		SpiOpenPort(0);
+
+		// Retrieving data
+		reset = 0;
+		for (j = 0; j < 60; j++) {
+			j = transfer(spi_cs0_fd, j); // j:=Packet number
+
+			if (j < 0) {
+				reset += 1;
+				usleep(10000);
+			}
+
+			if (reset == 750) {
+				SpiClosePort(0);
+				printf("Need to reset.\n");
+				//usleep(7500000);
+				SpiOpenPort(0);
+				break;
+			}
+			//printf("%d\n",j);
+		}
+
+		printf("\n");
+		IR_data = normalize(IR_data, lepton_image);
+		data.publish(IR_data);
+		ROS_INFO("Successfully published IR image!");
+		ros::spinOnce();
+
+		SpiClosePort(0);
+
+	}
+
+	return ret;
+}
